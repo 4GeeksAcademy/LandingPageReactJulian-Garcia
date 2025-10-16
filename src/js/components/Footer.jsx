55c48f267623067aa9131be3535d3b4191c151cc
juliangarciag05 <juliangarciag05@gmail.com>
2025-10-16 01:08:40 +0000

    termine footer y navbar

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
     return(
         <div>
-
+<footer className="bg-black text-light text-center py-3 mt-auto">
+  <div className="container">
+    <p className="mb-0"> Copyright &copy;. Your Website.</p>
+  </div>
+</footer>
         </div>
     )
 }
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,6 +6,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./Navbar.jsx"
 import Banner from "./Banner.jsx"
 import Card from "./Card.jsx"
+import Footer from "./Footer.jsx"
 
 //create your first component
 
@@ -13,10 +14,10 @@ import Card from "./Card.jsx"
 const Home = () => {
 
 	const birds = [
-		{ name: "El secretario", description: "(Sagittarius serpentarius)", button: "Ver más" , img: "https://www.chapultepec.org.mx/wp-content/uploads/2020/05/aves-mas-raras-exoticas-.jpg" },
-		{ name: "El azulejo de las montañas", description: "(Sialia currucoides)", button: "Conoce más" , img: "https://www.chapultepec.org.mx/wp-content/uploads/2020/05/aves-mas-raras-exoticas-8.jpg" },
-		{ name: "El bigotudo ", description: "(Panurus biarmicus)", button: "Mira más de su bigote" , img: "https://www.chapultepec.org.mx/wp-content/uploads/2020/05/aves-mas-raras-exoticas-9.jpg" },
-		{ name: "Quetzal", description: "(Pharomachrus)", button: "Conoce sus detalles", img:"https://desinformemonos.org/wp-content/uploads/2017/11/El-Quetzal.jpg"},
+		{ name: "El secretario", description: "(Sagittarius serpentarius)", button: "Ver más", img: "https://www.chapultepec.org.mx/wp-content/uploads/2020/05/aves-mas-raras-exoticas-.jpg" },
+		{ name: "El azulejo de las montañas", description: "(Sialia currucoides)", button: "Conoce más", img: "https://www.chapultepec.org.mx/wp-content/uploads/2020/05/aves-mas-raras-exoticas-8.jpg" },
+		{ name: "El bigotudo ", description: "(Panurus biarmicus)", button: "Mira más de su bigote", img: "https://www.chapultepec.org.mx/wp-content/uploads/2020/05/aves-mas-raras-exoticas-9.jpg" },
+		{ name: "Quetzal", description: "(Pharomachrus)", button: "Conoce sus detalles", img: "https://desinformemonos.org/wp-content/uploads/2017/11/El-Quetzal.jpg" },
 	]
 
 	return (
@@ -24,13 +25,14 @@ const Home = () => {
 			<Navbar />
 			<Banner />
 			<div className="container-fluid text-center m-auto " >
-				<div className="row" style={{justifyContent : "space-between"}}>
+				<div className="row" style={{ justifyContent: "space-between" }}>
 					{birds.map((item, index) => (
 
 						<Card key={index} name={item.name} description={item.description} img={item.img} button={item.button} />
 					))}
 				</div>
 			</div>
+			<Footer />
 		</div>
 	);
 };
